5d04b99db5f344e709a983557701749ff7021093
EleahBurman <eleahjburman@gmail.com>
2023-04-11 14:50:17 -0400

    Create express lab using students

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Smart Student', done: true, _id: 125223},
+  {text: 'Silly Student', done: false, _id: 127904},
+  {text: 'Sleepy Student', done: false, _id: 139608},
+]
+
+export { 
+	students
+}
